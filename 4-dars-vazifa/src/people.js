a18707a6c7cb8647d8d2a0013eb2e3c8d6ca677c
sanjarcik <sanjarbekarabboyev22@gmail.com>
2025-06-16 23:20:17 +0500

    tugatdim

--- 4-dars-vazifa/src/people.js
+++ 4-dars-vazifa/src/people.js
@@ -1,32 +1,37 @@
-import img from "./assets/sanjar.jpg"
+import img from "./assets/people.png"
 
 
 const people = [
     {
+        id: 1,
+        name: "Azamjon",
+        role: "Fronted o'quvchisi",
+        image: img,
+        comment: "Men Algoritm o‘quv markazida ta’lim olayotganimdan juda mamnunman. Bu yerda darslar juda tushunarli va sifatli tarzda o‘tiladi"
+    },
+    {
+        id: 2,
         name: "Sanjarbek",
         role: "Fronted o'quvchisi",
         image: img,
         comment: "Men Algoritm o‘quv markazida ta’lim olayotganimdan juda mamnunman. Bu yerda darslar juda tushunarli va sifatli tarzda o‘tiladi"
     },
     {
+        id: 3,
         name: "Sanjarbek",
         role: "Fronted o'quvchisi",
         image: img,
         comment: "Men Algoritm o‘quv markazida ta’lim olayotganimdan juda mamnunman. Bu yerda darslar juda tushunarli va sifatli tarzda o‘tiladi"
     },
     {
+        id: 4,
         name: "Sanjarbek",
         role: "Fronted o'quvchisi",
         image: img,
         comment: "Men Algoritm o‘quv markazida ta’lim olayotganimdan juda mamnunman. Bu yerda darslar juda tushunarli va sifatli tarzda o‘tiladi"
     },
     {
-        name: "Sanjarbek",
-        role: "Fronted o'quvchisi",
-        image: img,
-        comment: "Men Algoritm o‘quv markazida ta’lim olayotganimdan juda mamnunman. Bu yerda darslar juda tushunarli va sifatli tarzda o‘tiladi"
-    },
-    {
+        id: 5,
         name: "Sanjarbek",
         role: "Fronted o'quvchisi",
         image: img,
